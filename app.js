9214568ac9280a2b4b1eeefdb73b54032e8ba1ea
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-02-13 15:37:28 -0500

    created capture input functions

---
+++ app.js
@@ -0,0 +1,8 @@
+const profileDataArr = process.argv.slice(2, process.argv.length);
+console.log(profileDataArr);
+
+const printProfileData = (profileDataArr) => {
+    profileDataArr.forEach((profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArr);
